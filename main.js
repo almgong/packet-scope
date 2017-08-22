75c8538dd9917e2a0a417c12b44078eb52c506ad
Allen Gong <alm.gong@gmail.com>
2017-08-21 20:09:48 -0700

    Add initial UI components

---
+++ main.js
@@ -0,0 +1,53 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+/**
+ * Main entry point for the electron app. This file should contain/invoke the core logic 
+ * that registers windows and handles appropriate events.
+**/
+
+// convenience config object
+let config = {
+	appWindowWidth: 800,
+	appWindowHeight: 600,
+	appMainWindowPath: path.join(__dirname, 'views/index.html')
+};
+
+// Main window
+let win;
+
+/**
+ * Creates and loads the main window
+**/
+function createPacketScopeMainWindow() {
+	win = new BrowserWindow({
+		width: config.appWindowWidth,
+		height: config.appWindowHeight
+	});
+
+	win.loadURL(url.format({
+		pathname: config.appMainWindowPath,
+		protocol: 'file',
+		slashes: true
+	}));
+
+	win.webContents.openDevTools();
+}
+
+/* Register events */
+
+app.on('ready', createPacketScopeMainWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (win === null) {
+		createPacketScopeMainWindow();
+	}
+});
+
