82559256d43e8a3f5365a5ad64296cf120dd731f
Allen Gong <alm.gong@gmail.com>
2017-08-26 00:01:15 -0700

    Begin converting files to use 2 spaces for indentation

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const MainIpc = require('./lib/ipc/main');
 const path = require('path');
 const url = require('url');
 
-const mainIpc = new MainIpc();
-
 /**
  * Main entry point for the electron app. This file should contain/invoke the core logic 
  * that registers windows and handles appropriate events.
@@ -12,9 +10,9 @@ const mainIpc = new MainIpc();
 
 // convenience config object
 let config = {
-	appWindowWidth: 800,
-	appWindowHeight: 600,
-	appMainWindowPath: path.join(__dirname, 'views/index.html')
+  appWindowWidth: 800,
+  appWindowHeight: 600,
+  appMainWindowPath: path.join(__dirname, 'views/index.html')
 };
 
 // Main window
@@ -24,18 +22,18 @@ let win;
  * Creates and loads the main window
 **/
 function createPacketScopeMainWindow() {
-	win = new BrowserWindow({
-		width: config.appWindowWidth,
-		height: config.appWindowHeight
-	});
+  win = new BrowserWindow({
+    width: config.appWindowWidth,
+    height: config.appWindowHeight
+  });
 
-	win.loadURL(url.format({
-		pathname: config.appMainWindowPath,
-		protocol: 'file',
-		slashes: true
-	}));
+  win.loadURL(url.format({
+    pathname: config.appMainWindowPath,
+    protocol: 'file',
+    slashes: true
+  }));
 
-	win.webContents.openDevTools();
+  win.webContents.openDevTools();
 }
 
 /* Register events */
@@ -43,15 +41,15 @@ function createPacketScopeMainWindow() {
 app.on('ready', createPacketScopeMainWindow);
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-	if (win === null) {
-		createPacketScopeMainWindow();
-	}
+  if (win === null) {
+    createPacketScopeMainWindow();
+  }
 });
 
 // TODO move into its own module
@@ -59,18 +57,18 @@ const network = require('network');
 const pcap = require('pcap');
 
 network.get_active_interface((err, obj) => {
-	if (!err) {
-		let pcap_session = pcap.createSession(obj.name, '');	// '' for filter means log all packets
-		 pcap_session.on('packet', (raw_packet) => {
-		 	let packet = pcap.decode.packet(raw_packet);
+  if (!err) {
+    let pcap_session = pcap.createSession(obj.name, '');  // '' for filter means log all packets
+     pcap_session.on('packet', (raw_packet) => {
+      let packet = pcap.decode.packet(raw_packet);
 
-		 	console.log('packet!');
-		 	console.log(packet)
+      console.log('packet!');
+      console.log(packet);
 
-		 });
-	} else {
-		console.log('Error in getting active interface')
-	}
+     });
+  } else {
+    console.log('Error in getting active interface')
+  }
 });
 
 
